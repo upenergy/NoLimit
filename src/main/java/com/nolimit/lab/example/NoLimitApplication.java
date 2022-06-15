1aa704f8d16a724c950cc8018a6da394e5557aaa
AirFormulate <arrayq@protonmail.com>
2022-06-15 00:43:46 -0700

    Add ComponentScan

--- src/main/java/com/nolimit/lab/example/NoLimitApplication.java
+++ src/main/java/com/nolimit/lab/example/NoLimitApplication.java
@@ -2,10 +2,12 @@ package com.nolimit.lab.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@ComponentScan("com.nolimit")
 public class NoLimitApplication {
     public static void main(String[] args) {
         SpringApplication.run(NoLimitApplication.class, args);
