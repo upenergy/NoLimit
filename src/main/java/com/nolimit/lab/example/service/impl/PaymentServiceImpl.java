960ca870a526b6f0a954ab025b07814e44479a47
AirFormulate <arrayq@protonmail.com>
2022-06-29 02:39:57 -0700

    Upload

--- src/main/java/com/nolimit/lab/example/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/nolimit/lab/example/service/impl/PaymentServiceImpl.java
@@ -2,7 +2,13 @@ package com.nolimit.lab.example.service.impl;
 
 import com.nolimit.lab.example.service.PaymentService;
 
+@RequestMapping("/payments")
 public class PaymentServiceImpl implements PaymentService {
 
     private static final int DEFAULT_PAYMENT_METHOD = 0;
+
+    @RequestMapping("/list")
+    public List<PaymentOrder> listPaymentOrder(@RequestBody PaymentOrderDTO paymentOrderDTO) {
+
+    }
 }
