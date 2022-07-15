eb29d1eef6868b7418379f7b10a55d18147642f5
AirFormulate <arrayq@protonmail.com>
2022-07-15 02:25:23 -0700

    Update payment order file

--- src/main/java/com/nolimit/lab/example/entity/po/PaymentOrder.java
+++ src/main/java/com/nolimit/lab/example/entity/po/PaymentOrder.java
@@ -1,2 +1,12 @@
-package com.nolimit.lab.example.entity.po;public class PaymentOrder {
+package com.nolimit.lab.example.entity.po;
+
+import java.io.Serializable;
+
+public class PaymentOrder implements Serializable {
+
+    private Long id;
+    private String orderNum;
+    private String outTradeNo;
+
+
 }
