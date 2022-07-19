c1be5a46e3f1eb5c4ca24b9968e7d13dc1b19fd7
AirFormulate <arrayq@protonmail.com>
2022-07-18 23:24:27 -0700

    Update PaymentService

--- src/main/java/com/nolimit/lab/example/service/PaymentService.java
+++ src/main/java/com/nolimit/lab/example/service/PaymentService.java
@@ -3,6 +3,10 @@ package com.nolimit.lab.example.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface PaymentService extends BaseService {
+public interface PaymentService {
 
+	
+    PageDTO<PaymentOrder> listPaymentOrder(@RequestBody PaymentOrderDTO paymentOrderDTO);
+
+    PaymentOrder getById(@PathVariable("id") PaymentOrderDTO paymentOrderDTO);
 }
