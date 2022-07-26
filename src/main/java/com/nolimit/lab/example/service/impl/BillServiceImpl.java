858bf63ab3110664e9e042c51bc9010eb8374f09
AirFormulate <arrayq@protonmail.com>
2022-07-25 18:22:20 -0700

    Update BillServiceImpl

--- src/main/java/com/nolimit/lab/example/service/impl/BillServiceImpl.java
+++ src/main/java/com/nolimit/lab/example/service/impl/BillServiceImpl.java
@@ -5,5 +5,11 @@ import com.nolimit.lab.example.service.BillService;
 
 public class BillServiceImpl implements BillService {
 
+	public List<Bill> listBill() {
+
+		List<Bill> billList = null;
+		return billList;
+	}
+
 
 }
