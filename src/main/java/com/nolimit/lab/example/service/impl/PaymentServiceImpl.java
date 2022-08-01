570eaf2ded2ee921baaf015806dd83a0888964a1
AirFormulate <arrayq@protonmail.com>
2022-08-01 02:47:52 -0700

    Update PaymentServiceImpl

--- src/main/java/com/nolimit/lab/example/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/nolimit/lab/example/service/impl/PaymentServiceImpl.java
@@ -3,12 +3,12 @@ package com.nolimit.lab.example.service.impl;
 import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
 import com.nolimit.lab.example.service.PaymentService;
 
-@RequestMapping("/payments")
+
 public class PaymentServiceImpl implements PaymentService extends BaseService {
 
     private static final int DEFAULT_PAYMENT_METHOD = 0;
 
-    @RequestMapping("/list")
+    
     public PageDTO<PaymentOrder> listPaymentOrder(@RequestBody PaymentOrderDTO paymentOrderDTO) {
 
         List<PaymentOrder> paymentOrderList = null;
