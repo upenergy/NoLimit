cab1700b6569631aa4b751e60f71b7208b0d121c
AirFormulate <arrayq@protonmail.com>
2022-08-26 04:20:01 -0700

    Append variable

--- src/main/java/com/nolimit/lab/example/entity/po/PaymentOrder.java
+++ src/main/java/com/nolimit/lab/example/entity/po/PaymentOrder.java
@@ -1,12 +1,19 @@
 package com.nolimit.lab.example.entity.po;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
 public class PaymentOrder implements Serializable {
 
     private Long id;
     private String orderNum;
+    private BigDecimal price;
     private String outTradeNo;
-
+    private BigDecimal salePrice;
+    private OrderState orderState;
+    
 
 }
