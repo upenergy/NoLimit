f3cea570cdd730bcde1e4bb723846f456a638baa
AirFormulate <arrayq@protonmail.com>
2022-09-21 19:19:32 -0700

    Append PaymentOrderMapper file

---
+++ src/main/java/com/nolimit/lab/example/mapper/PaymentOrderMapper.java
@@ -0,0 +1,9 @@
+public interface PaymentOrderMapper {
+    
+    @Select("SELECT * FROM payment_order")
+    List<PaymentOrder> getAll();
+
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    @Insert("INSERT INTO payment_order (out_trade_no, order_number, version, created_at) VALUES (#{paymentOrder.outTradeNo}, #{paymentOrder.orderNumber}, #{paymentOrder.version}, #{paymentOrder.createdAt})")
+    void insert(@Param("paymentOrder") PaymentOrder paymentOrder);
+}
