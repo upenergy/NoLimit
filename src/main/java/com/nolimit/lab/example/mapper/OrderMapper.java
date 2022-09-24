adcbe351db60a71aa6f5669cdd73c895ef814dcb
AirFormulate <arrayq@protonmail.com>
2022-09-23 22:50:54 -0700

    Update OrderMapper

--- src/main/java/com/nolimit/lab/example/mapper/OrderMapper.java
+++ src/main/java/com/nolimit/lab/example/mapper/OrderMapper.java
@@ -1,6 +1,6 @@
 public interface OrderMapper {
     
-    findOrders();
-    findOrderById();
+    @Select("SELECT * FROM orders")
+    List<Order> getAll(@Param("offset") int offset, @Param("maxResults") int maxResults);
     
 }
