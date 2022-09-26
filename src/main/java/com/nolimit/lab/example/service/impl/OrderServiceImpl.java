0ae864bb636d8364f2619cb5db24768a9937e37d
AirFormulate <arrayq@protonmail.com>
2022-09-26 01:42:07 -0700

    Update OrderServiceImpl

--- src/main/java/com/nolimit/lab/example/service/impl/OrderServiceImpl.java
+++ src/main/java/com/nolimit/lab/example/service/impl/OrderServiceImpl.java
@@ -4,7 +4,12 @@ public class OrderServiceImpl implements OrderService {
     @Resource
     private IOrderDAO orderDAO;
 
+    @Resource
+    private OrderMapper orderMapper;
+
     public List<Order> listOrder() {
         
+        OrderFilter orderFilter = new OrderFilter();
+        return orderMapper.listOrder(OrderFilter);
     }
 }
