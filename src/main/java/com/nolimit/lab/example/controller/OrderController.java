a938fbf7cbaad6eb320d63ad667605e378df6cb7
AirFormulate <arrayq@protonmail.com>
2022-10-03 00:17:16 -0700

    Update OrderController

--- src/main/java/com/nolimit/lab/example/controller/OrderController.java
+++ src/main/java/com/nolimit/lab/example/controller/OrderController.java
@@ -2,5 +2,14 @@
 @RestController
 @RequestMapping("/order")
 public OrderController extends BaseController {
-    
+
+    @Autowired
+    private OrderService orderService;
+
+    public List<Order> listOrder() {
+        
+
+        OrderFilter orderFilter = new OrderFilter();
+        return orderService.getOrderList(OrderFilter);
+    }
 }
