7d0f0e0acccc2e3e6ff3b3b24f1701e972c1f10e
arrayq <arrayq@protonmail.com>
2022-10-07 18:45:41 +0800

    Update OrderMapper

--- src/main/java/com/nolimit/lab/example/mapper/OrderMapper.java
+++ src/main/java/com/nolimit/lab/example/mapper/OrderMapper.java
@@ -3,4 +3,5 @@ public interface OrderMapper {
     @Select("SELECT * FROM orders")
     List<Order> getAll(@Param("offset") int offset, @Param("maxResults") int maxResults);
     
+    PageDTO<Order> listOrderByPage(Long start, Integer pageSize, OrderDTO order);
 }
