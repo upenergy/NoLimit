1ce01373125522f0eb5f42ecfcd081db45ac7e4e
AirFormulate <arrayq@protonmail.com>
2022-10-14 08:24:32 -0700

    Update PaymentOrderService.java

--- src/main/java/com/nolimit/lab/example/service/PaymentOrderService.java
+++ src/main/java/com/nolimit/lab/example/service/PaymentOrderService.java
@@ -1,4 +1,13 @@
 package com.nolimit.lab.example.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface PaymentOrderService {
+
+	
+    PageDTO<PaymentOrder> listPaymentOrder(@RequestBody PaymentOrderDTO paymentOrderDTO);
+
+    PaymentOrder getById(@PathVariable("id") PaymentOrderDTO paymentOrderDTO);
+    
 }
