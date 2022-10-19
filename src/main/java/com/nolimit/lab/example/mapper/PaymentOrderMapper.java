786c300c28f237fe7f984ef4b6db6884f8eedef3
AirFormulate <arrayq@protonmail.com>
2022-10-19 09:47:55 -0700

    Update PaymentOrderMapper.java

--- src/main/java/com/nolimit/lab/example/mapper/PaymentOrderMapper.java
+++ src/main/java/com/nolimit/lab/example/mapper/PaymentOrderMapper.java
@@ -7,6 +7,9 @@ public interface PaymentOrderMapper {
     @Insert("INSERT INTO payment_order (out_trade_no, order_number, version, created_at) VALUES (#{paymentOrder.outTradeNo}, #{paymentOrder.orderNumber}, #{paymentOrder.version}, #{paymentOrder.createdAt})")
     void insert(@Param("paymentOrder") PaymentOrder paymentOrder);
 
-    
+    @Select("SELECT * FROM payment_order")
+    List<PaymentOrder> getOrderList(PaymentOrderDTO paymentOrderDTO);
+
+
     PaymentOrder update(@Param("paymentOrder") PaymentOrder paymentOrder);
 }
