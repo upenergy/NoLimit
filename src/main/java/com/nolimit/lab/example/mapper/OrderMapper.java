ab31883040c4cd18ca0d7a42f9c0c999e8337f83
AirFormulate <arrayq@protonmail.com>
2022-10-20 10:07:11 -0700

    Update OrderMapper.java

--- src/main/java/com/nolimit/lab/example/mapper/OrderMapper.java
+++ src/main/java/com/nolimit/lab/example/mapper/OrderMapper.java
@@ -2,10 +2,13 @@ public interface OrderMapper {
     
     @Select("SELECT * FROM orders")
     List<Order> getAll(@Param("offset") int offset, @Param("maxResults") int maxResults);
+
+
+    @Select("SELECT * FROM orders")
+    List<Order> listOrder();
     
     @Select("SELECT * FROM orders")
-    PageDTO<Order> listOrderByPage(Long start, Integer pageSize, OrderDTO order);
-    
-    @Select("SELECT * FROM orders WHERE id:=orderId")
-    Order getById(Long orderId);
+    PageDTO<Order> findOrderByPage(Map<String, Object> param);
+
+    Long totalNumber();
 }
